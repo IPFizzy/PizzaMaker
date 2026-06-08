a22f115288cff580cf51340fa28c8e9f50ce6285
IPFizzy <fizzy@keonbushman.com>
2026-06-08 15:20:34 -0700

    Add a class library and pizza DAO and pizza logic classes.

---
+++ PizzaMakerClassLibrary/Services/DataAccessLayer/PizzaDAO.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Keon Bushman
+ * CST - 250
+ * 06/14/2026
+ * Pizza Maker
+ * Activity 4
+ * Activity 4 Guide
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaMakerClassLibrary.Services.DataAccessLayer
+{
+    public class PizzaDAO
+    {
+        // Class level variables
+        private List<PizzaModel> _pizzaOrder;
+
+        /// <summary>
+        /// Default constructor for the pizza DAO
+        /// </summary>
+        public PizzaDAO()
+        {
+            // Initialize the _pizzaOrder list
+            _pizzaOrder = new List<PizzaModel>();
+        }
+    }
+}
